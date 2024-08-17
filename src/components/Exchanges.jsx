8c886f2356688def8d118a3f02c7360ce4178498
SHRYNSH-NETAM <netams2000@gmail.com>
2024-08-17 10:06:39 +0530

    Final cryptoverse is complete

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exchanges = () => {
+  return (
+    <div>
+        Exchanges Build in Progress....
+    </div>
+  )
+}
+
+export default Exchanges
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '341e11290emsh41b002da2d9fb76p1f5cdejsn3d4883354008',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
